3d227a9217029449d6d85797920e995e293696e5
【daiki】 <daiki20010705@icloud.com>
2021-08-22 18:36:06 +0900

    a

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
   <title>POSSE課題 Quizy</title>
   <link rel="stylesheet" href="./styles.css">
 
-  <!-- bootstrap -->
+  <!-- bootstrap --> 
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
 </head>
 
