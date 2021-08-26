c70daa3c1e83d502034ff1372d3e643c8f0fb0dc
【daiki】 <daiki20010705@icloud.com>
2021-08-26 17:25:50 +0900

    一旦コミット

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,15 @@
-// window.onload = alert("a");
+// let correct_answer = document.getElementById = ("question__lists__1");
 
+// let clickAction = function () {
+// }
+
+  $('.question__list__1').click(function() {
+    console.log('正解がクリックされました！');
+    $('.question__list__1').addClass("succeeded");
+  })
+
+  $('.question__list__0').click(function() {
+    console.log('不正解がクリックされました！');
+    $('.question__list__0').addClass("failed");
+
+  })
