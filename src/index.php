d26bdf377bb690684d594ad0ffe48c9bd7a17e7a
【daiki】 <daiki20010705@icloud.com>
2021-08-26 14:32:55 +0900

    scss導入

--- src/index.php
+++ src/index.php
@@ -32,12 +32,12 @@ if(isset($_GET['id'])) {
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ja">
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>くいじー</title>
 </head>
 <body>
 
