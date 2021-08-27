2284b12e041d6be5c0db48b836a94e5a0e054a91
【daiki】 <daiki20010705@icloud.com>
2021-08-28 04:43:13 +0900

    一旦完成

--- src/index.php
+++ src/index.php
@@ -6,29 +6,33 @@ $password = 'pass';
 try {
   $db = new PDO($dsn, $user, $password);
   $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch(PDOException $e) {
+} catch (PDOException $e) {
   echo '接続失敗: ' . $e->getMessage();
   exit();
 }
 
 // questions
-if(isset($_GET['id'])) {
+if (isset($_GET['id'])) {
   $id = $_GET['id'];
   $questions_value =  "SELECT * FROM questions WHERE id = $id";
-  $questions = $db->query($questions_value)->fetchAll(PDO::FETCH_ASSOC|PDO::FETCH_UNIQUE);
+  $questions = $db->query($questions_value)->fetchAll(PDO::FETCH_ASSOC | PDO::FETCH_UNIQUE);
 }
-echo($questions[$id]["name"]);
+echo ($questions[$id]["name"]);
 
 // choices
-if(isset($_GET['id'])) {
+if (isset($_GET['id'])) {
   $id = $_GET['id'];
   $choices_value =  "SELECT * FROM choices WHERE question_id = $id";
-  $choices = $db->query($choices_value)->fetchAll(PDO::FETCH_ASSOC|PDO::FETCH_UNIQUE);
+  $choices = $db->query($choices_value)->fetchAll(PDO::FETCH_ASSOC | PDO::FETCH_UNIQUE);
+
+  $choices_corrects =  "SELECT * FROM choices WHERE question_id = $id AND valid = 1";
+  $corrects = $db->query($choices_corrects)->fetchAll(PDO::FETCH_ASSOC | PDO::FETCH_UNIQUE);
 }
 ?>
 
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -36,34 +40,50 @@ if(isset($_GET['id'])) {
   <title>くいじー</title>
   <link rel="stylesheet" href="./css/style.css">
 </head>
+
 <body>
 
-<div class="question">
-  <h1 class="question__title">
-    <?php // 問題番号動的 TODO:コード汚いので後できれいにする
+  <div class="question">
+    <h1 class="question__title">
+      <?php // 問題番号動的 TODO:コード汚いので後できれいにする
       foreach ($choices as $choice) {
-        echo($choice['question_id']);
-        if($choice == $choice){
+        echo ($choice['question_id']);
+        if ($choice == $choice) {
           break;
         }
-      } 
-    ?> 
-    .この地名はなんて読む？
-  </h1>
+      }
+      ?>
+      .この地名はなんて読む？
+    </h1>
 
 
-  <img class="question__img" src="./img/<?php echo $id ?>.png" alt="選択肢の写真">
+    <img class="question__img" src="./img/<?php echo $id ?>.png" alt="選択肢の写真">
 
-  <ul class="question__lists">
-      <?php foreach ( $choices as $choice ) { ?>
-      <li class="question__list question__list__<?php echo $choice['valid'] ?>"><?php echo $choice['name'];?></li>
-      <?php  
-      }?>
-  </ul>
-</div>
+    <ul class="question__lists">
+      <?php foreach ($choices as $index => $choice) { ?>
+        <li class="question__list">
+          <?php echo $choice['name']; ?>
+        </li>
+      <?php
+      } ?>
+    </ul>
 
-<!-- jquery -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-<script src="./js/index.js"></script>
+    <div class="question__answer">
+      <p class="question__answer__text">正解！</p>
+      <p class="question__answer__text__choice">
+        正解は
+        <?php foreach ($corrects as $correct) {
+          echo ($correct['name']); 
+        }?>
+        です！
+      </p>
+    </div>
+
+  </div>
+
+  <!-- jquery -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="./js/index.js"></script>
 </body>
+
 </html>
