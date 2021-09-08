5d231c44cb4a5e4c9fb28151aece02613b88c544
【daiki】 <daiki20010705@icloud.com>
2021-09-08 19:28:29 +0900

    一旦コミット

--- src/index.php
+++ src/index.php
@@ -6,16 +6,8 @@ include($_SERVER['DOCUMENT_ROOT'] . "/php/_inc/db_connect.php");
 
 <main>
   <?php include($_SERVER['DOCUMENT_ROOT'] . "/php/_inc/components/organisms/learning__time.php");?>
-  <?php include($_SERVER['DOCUMENT_ROOT'] . "/php/_inc/components/organisms/learning__graph.php");?>
   <?php include($_SERVER['DOCUMENT_ROOT'] . "/php/_inc/components/organisms/learning__chart.php");?>
+  <?php include($_SERVER['DOCUMENT_ROOT'] . "/php/_inc/components/organisms/learning__graph.php");?>
 </main>
 
-
-
-
-  <!-- jquery -->
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-  <script src="./js/index.js"></script>
-</body>
-
-</html>
+<?php include($_SERVER['DOCUMENT_ROOT'] . "/php/_inc/components/organisms/footer.php");?>
--- src/php/_inc/components/organisms/learning__chart.php
+++ src/php/_inc/components/organisms/learning__chart.php
@@ -1,9 +1,31 @@
+<?php 
+
+include($_SERVER['DOCUMENT_ROOT'] . "/php/_inc/db_connect.php");
+
+// 今日の日付のデータを取得
+
+
+// $todeay_study_value = "SELECT date_format(["study_date"],'%Y%m%d'),count(["learning_time"]) FROM study GROUP BY date_format(["study_date"],'%Y%m%d')";
+
+
+
+
+
+$today_studies = $db->query($toeday_study_value)->fetch();
+
+
+
+// SELECT date_format(["study_date"],'%Y%m%d'),count(["learning_time"]) FROM ["study"] GROUP BY date_format(["study_date"],'%Y%m%d');
+
+
+
+
+?>
+
 <section class="learning__chart">
-  <div class="learning__chart__items">
-    <div class="learning__chart__item">
+<!-- <div id="chart_div" style="width:100%"></div> -->
 
-    </div>
 
-  </div>
 
+<div id="gct_sample_column" style="width:80%; height:250pt" ></div>
 </section>
