3e70161ba64795568c4cf769095e119ba97882c3
【daiki】 <daiki20010705@icloud.com>
2022-06-13 13:39:30 +0900

    Improve: くいじーの見た目修正

--- src/resources/views/quizy/quiz.blade.php
+++ src/resources/views/quizy/quiz.blade.php
@@ -1,16 +1,22 @@
 @extends('layouts.index')
 
 @section('contents')
-    @foreach ($questions as $question)
-        <div class="container">
-            <h3>この地名はなんと読む</h3>
-            <ul
-                class="w-48 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white">
-                @foreach ($question->choices as $choice)
-                    <li class="w-full px-4 py-2 border-b border-gray-200 rounded-t-lg dark:border-gray-600">
-                        {{ $choice->name }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endforeach
+    <div class="questions flex justify-center flex-col items-center">
+        @foreach ($questions as $question)
+            <div class="question">
+                <h3 class="font-bold">{{ $loop->iteration }}.この地名はなんと読む</h3>
+                <img src="/img/takanawa.png" alt="問題の写真">
+                <ul class="w-auto text-sm font-medium text-gray-900">
+                    @foreach ($question->choices as $choice)
+                        <button
+                            class="w-full hover:bg-gray-100 font-bold py-2 px-4 rounded mb-5 border border-gray-400 rounded-lg">
+                            <li class="w-full px-4 py-2">
+                                {{ $choice->name }}
+                            </li>
+                        </button>
+                    @endforeach
+                </ul>
+            </div>
+        @endforeach
+    </div>
 @endsection
