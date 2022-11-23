cf8c6110b58518b832aa11a2007d41e797c817f6
【daiki】 <daiki20010705@icloud.com>
2022-11-24 02:18:11 +0900

    feat: :zap: crate study bar chart

--- front/src/pages/index.tsx
+++ front/src/pages/index.tsx
@@ -1,12 +1,41 @@
 import { Box } from '@mui/material';
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 
 import StudyingHour from '@/components/organisms/StudyingHour';
-
+import VerticalBarChart from '@/components/organisms/VerticalBarChart';
+import { OneMonthStudyTimeInner, StudyingHourApi } from '@/generated/types/typescript-axios';
+import { MonthYearContext } from '@/layout/Layout';
+import { ApiConfig } from '@/utils/apiConfig';
 const Home: React.FC = () => {
+  const value = useContext(MonthYearContext);
+  const yearId = value?.thisYear;
+  const monthId = value?.thisMonth;
+  const [oneMonthStudyTimes, setOneMonthStudyTimes] = useState<OneMonthStudyTimeInner[]>([
+    {
+      id: 0,
+      date: 0,
+      study_hour: 0,
+    },
+  ]);
+
+  const fetchOneMonthStudyTime = async (yearId: number, monthId: number) => {
+    try {
+      const apiConfig = ApiConfig();
+      const studyingHourApi = new StudyingHourApi(apiConfig);
+      const { data } = await studyingHourApi.getOneMonthStudyTime(yearId, monthId);
+      setOneMonthStudyTimes(data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchOneMonthStudyTime(yearId, monthId);
+  }, []);
   return (
     <Box sx={{ p: 2, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
       <StudyingHour />
+      <VerticalBarChart oneMonthStudyTimes={oneMonthStudyTimes} />
     </Box>
   );
 };
